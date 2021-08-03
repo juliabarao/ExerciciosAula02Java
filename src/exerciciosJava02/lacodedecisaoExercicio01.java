64a8a7d14bf91991640da91a39e6d3df054aaeec
Julia <juliarbaraos@gmail.com>
2021-08-03 17:31:09 -0300

    Adicionando arquivo de exercicios 02

---
+++ src/exerciciosJava02/lacodedecisaoExercicio01.java
@@ -0,0 +1,30 @@
+package exerciciosJava02;
+
+import java.util.Scanner;
+
+public class lacodedecisaoExercicio01 {
+
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+		
+		int n1, n2, n3, maior;
+		
+		System.out.println("Digite três números: ");
+		n1 = ler.nextInt();
+		n2 = ler.nextInt();
+		n3 = ler.nextInt();
+		
+		if(n1 > n2 && n1 > n3) {
+			System.out.println("O maior número é: " +n1);
+		}
+		else if(n2 > n1 && n2 > n3) {
+			System.out.println("O maior número é: " +n2);
+		}
+		else if(n3 > n1 && n3 > n2) {
+			System.out.println("O maior número é: " +n3);
+		}
+	ler.close();
+	}
+
+}
---
+++ src/exerciciosJava02/lacodedecisaoExercicio02.java
@@ -0,0 +1,34 @@
+package exerciciosJava02;
+
+import java.util.Scanner;
+
+public class lacodedecisaoExercicio02 {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int n1, n2, n3;
+		
+		System.out.println("Digite três números: ");
+		n1 = ler.nextInt();
+		n2 = ler.nextInt();
+		n3 = ler.nextInt();
+		
+		if(n1<= n2 && n2<=n3) {
+			System.out.println("A ordem crescente é: \n" + n1 + n2 + n3);
+		} else if(n1<= n3 && n3<=n2) {
+			System.out.println("A ordem crescente é: \n" + n1 + n3 + n2);
+		}else if(n2<= n1 && n1<=n3) {
+			System.out.println("A ordem crescente é: \n" + n2 + n1 + n3);
+		}else if(n2 <= n3 && n3 <= n1) {
+			System.out.println("A ordem crescente é: \n" + n2 + n3 + n1);
+		}else if(n3 <= n1 && n1 <= n2) {
+			System.out.println("A ordem crescente é: \n" + n3 + n1 + n2);
+		}else {
+			System.out.println("A ordem crescente é: \n" + n3 + n2 + n1);
+		}
+	ler.close();
+	}
+
+}
